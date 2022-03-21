673efa930411562325618a566e0cf21f0a0746be
Eduardo Vieira <>
2022-03-21 01:19:57 -0300

    Project renamed. Ping implemented. Layout changes.

--- pages/api/reachable.js
+++ pages/api/reachable.js
@@ -15,16 +15,28 @@ export default async function (req, res) {
             res.status(400).json(response);
         }
 
+        const protocols = {};
+
+        const request_protocols_gist = await (await fetch("https://api.github.com/gists/e073b0eabfd21dca35e4517a470f69f5")).json();
+        if (request_protocols_gist.files) {
+            const request_protocols = request_protocols_gist.files["protocols.ini"].content;
+            if (request_protocols) {
+                request_protocols.split("\n").map(line => {
+                    const [key, value] = line.split("=");
+                    protocols[key] = value;
+                });
+            }
+        }
+
         let url_parts = params.host.split("://").reverse();
         const protocol = url_parts[1] || "http";
         const [host, url_port] = url_parts[0].split(":");
         var port = params.port || url_port;
 
-        if (!port && protocol === "https") port = 443;
-        if (!port && (protocol === "http" || protocol === "ws")) port = 80;
-        if (!port && protocol === "mysql") port = 3306;
+        if (!port) port = protocols[protocol] || 80;
 
         response.target = { protocol, host, port };
+
 
         await isReachable(`${protocol}://${host}:${port}`).then(reachable => {
             response.reachable = reachable;
@@ -33,6 +45,14 @@ export default async function (req, res) {
             console.log(err);
             response.message = err.message;
         });
+
+        if (response.reachable) {
+            var time = new Date().getTime();
+            await isReachable(`${protocol}://${host}:${port}/`).finally(() => {
+                time = new Date().getTime() - time;
+                response.target.time = time;
+            });
+        }
 
         return res.status(response.statusCode || 200).json({ ...response });
     }
--- pages/index.jsx
+++ pages/index.jsx
@@ -13,23 +13,22 @@ export default function HomePage({ host, ...props }) {
     const [is_reachable, setReachable] = useState(null);
     const [data, setData] = useState(null);
 
-    const [ping_form, setPingFormData] = useState({
-        host: host || "",
-    });
-    const handlePingFormInput = (e) => {
+    const [reachable_form, setReachableFormData] = useState({});
+    const handleReachableFormInput = (e) => {
         const { id, name, value } = e.target;
         const key = name !== "" ? name : id;
 
-        setPingFormData({ ...ping_form, [key]: value });
+        setReachableFormData({ ...reachable_form, [key]: value });
     };
 
-    const handlePingFormSubmit = (e) => {
+    const handleReachableFormSubmit = (e) => {
         e.preventDefault();
         toggleLoading(true);
         setReachable(null);
         setData(null);
 
-        axios.get(`/api/ping`, { params: ping_form, timeout: 1000 * 60 }).then(({ data }) => {
+        axios.get(`/api/reachable`, { params: reachable_form, timeout: 1000 * 60 }).then(({ data }) => {
+            console.log(data);
             setData(data.target);
             setReachable(data.reachable);
         }).catch(err => {
@@ -42,19 +41,19 @@ export default function HomePage({ host, ...props }) {
 
     return <main className="d-flex flex-column flex-fill gap-3">
         <hgroup className="container-fluid p-3 bg-primary text-light">
-            <h1 className="fw-bold">Ping</h1>
-            <p className="lead">A tool to test if host and port are public and open.</p>
+            <h1 className="fw-bold">Openport</h1>
+            <p className="lead">A tool to test if host and port are public.</p>
         </hgroup>
         <div className="container-fluid d-flex flex-column gap-3 px-3">
-            <form onSubmit={handlePingFormSubmit}>
+            <form onSubmit={handleReachableFormSubmit}>
                 <fieldset className="row g-2">
                     <div className="col-md-8 col-lg-4 d-flex flex-column gap-2">
                         <div className="input-group">
-                            <span className="input-group-text"><i className="fal fa-globe" /></span>
-                            <input type="search" name="host" id="input-host" value={ping_form.host ?? ''} onChange={handlePingFormInput} className="form-control" placeholder="Host" required autoFocus={true} autoComplete="off" autoCorrect="off" autoCapitalize="off" />
+                            <span className="input-group-text"><i className="fal fa-network-wired" /></span>
+                            <input type="search" name="host" id="input-host" value={reachable_form.host ?? ''} onChange={handleReachableFormInput} className="form-control" placeholder="Host" required autoFocus={true} autoComplete="off" autoCorrect="off" autoCapitalize="off" />
 
                             <button type="submit" className={`btn btn-${is_reachable === true ? "success" : is_reachable === false ? "danger" : "primary"} px-3`} disabled={loading}>
-                                Test {loading && <i className="fal fa-spinner-third fa-spin" />}
+                                <b>try</b> {loading && <i className="fal fa-spinner-third fa-spin" />}
                                 {is_reachable === true && <i className="fal fa-check"></i>}
                                 {is_reachable === false && <i className="fal fa-times"></i>}
                             </button>
@@ -63,13 +62,22 @@ export default function HomePage({ host, ...props }) {
                         {data &&
                             <div className="card border p-2">
                                 <ul className="list-unstyled">
+                                    <li>Reachable: {is_reachable === true ? "Yes" : is_reachable === false ? "No" : "Unknown"}</li>
                                     <li>Protocol: {data.protocol}</li>
                                     <li>Host: {data.host}</li>
                                     <li>Port: {data.port}</li>
-                                    <li>Reachable: {is_reachable === true ? "Yes" : is_reachable === false ? "No" : "Unknown"}</li>
+                                    <li>Ping: {data.time || "*"}ms</li>
                                 </ul>
                             </div>
                         }
+
+                        <div className="card border p-2">
+                            <h2 className="h4 fw-bold">How to use:</h2>
+                            <p>Type <code>protocol://host:port</code> to test if is reachable. Some protocol points to default port numbers: <small className="text-muted">(click to use)</small></p>
+                            <ul className="list-inline d-flex flex-row gap-1 flex-wrap">
+                                {props.protocols && Object.keys(props.protocols).map((protocol) => <li key={protocol} className="btn btn-outline-primary btn-sm p-0 px-1" onClick={() => setReachableFormData({ ...reachable_form, host: `${protocol}://` })}>{protocol}: <code className="text-info">{props.protocols[protocol]}</code></li>)}
+                            </ul>
+                        </div>
                     </div>
                 </fieldset>
             </form>
@@ -82,5 +90,21 @@ export const getServerSideProps = async (ctx) => {
     const props = {};
     const host = ctx.req.headers['x-forwarded-for'] || ctx.req.connection.remoteAddress;
     props.host = host || null;
+
+    const protocols = {};
+
+    const request_protocols_gist = await (await fetch("https://api.github.com/gists/e073b0eabfd21dca35e4517a470f69f5")).json();
+    if (request_protocols_gist.files) {
+        const request_protocols = request_protocols_gist.files["protocols.ini"].content;
+        if (request_protocols) {
+            request_protocols.split("\n").map(line => {
+                const [key, value] = line.split("=");
+                protocols[key] = value;
+            });
+        }
+    }
+
+    props.protocols = protocols;
+
     return { props: props };
 };
